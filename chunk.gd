68c4684f99e244f706f344c6a368fd0b50e3f44c
Adrian Javier <javier.adrian.kld@gmail.com>
2024-01-02 00:25:16 +0800

    Add a 'Block' class

--- chunk.gd
+++ chunk.gd
@@ -27,7 +27,15 @@ var back: Array[int] =   [7, 5, 4, 6]
 
 var surfaceTool = SurfaceTool.new()
 
+var blocks: Dictionary
+
 func _ready():
+    generate()
+    update()
+
+
+## Generate the [member mesh] and [member collider]
+func update():
     surfaceTool.begin(Mesh.PRIMITIVE_TRIANGLES)
     for x in range(dimensions.x):
         for y in range(dimensions.y):
@@ -46,14 +54,16 @@ func _ready():
         }
         ))
 
-    # print(surface.surface_get_array_len(0))
-    # print(surface.get_surface_count())
 
-
-func update():
-    pass
+## Populates [member blocks] with [Block]s.
 func generate():
-    pass
+    var block: Block = Block.new()
+
+    for x in range(dimensions.x):
+        for y in range(dimensions.y):
+            for z in range(dimensions.z):
+                blocks[Vector3i(x,y,z)] = block
+
 func createBlockMesh(blockPosition: Vector3i):
     if checkTransparent(blockPosition + Vector3i.UP):
         createFaceMesh(top, blockPosition)
@@ -67,6 +77,7 @@ func createBlockMesh(blockPosition: Vector3i):
         createFaceMesh(front, blockPosition)
     if checkTransparent(blockPosition + Vector3i.BACK):
         createFaceMesh(back, blockPosition)
+
 func createFaceMesh(face: Array[int], blockPosition: Vector3i):
     var a = vertices[face[0]] + blockPosition
     var b = vertices[face[1]] + blockPosition
@@ -78,6 +89,7 @@ func createFaceMesh(face: Array[int], blockPosition: Vector3i):
 
     surfaceTool.add_triangle_fan(topLeftTriangle)
     surfaceTool.add_triangle_fan(bottomRightTriangle)
+
 func checkTransparent(blockPosition: Vector3i):
     if blockPosition.x < 0 || blockPosition.x >= dimensions.x: return true
     if blockPosition.y < 0 || blockPosition.y >= dimensions.y: return true
