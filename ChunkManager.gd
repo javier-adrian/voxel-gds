e7a11df063cf1c8ca392f70175040947266f65d5
Adrian Javier <javier.adrian.kld@gmail.com>
2024-01-21 07:41:01 +0800

    add some documentation to chunkManager.gd

--- ChunkManager.gd
+++ ChunkManager.gd
@@ -1,8 +1,11 @@
 extends Node
 
+## Dictionary of [Chunks] and their [i]positions[/i].
 var chunkToPosition: Dictionary
+## Dictionary of [i]positions[/i] and the [Chunk]s located in them.
 var positionToChunk: Dictionary
 
+## An array of the [Chunk]s in the world.
 var chunks: Array
 
 var chunkInstance = Chunk.new()
@@ -13,6 +16,7 @@ func _ready():
 	for i in chunks:
 		i = i as Chunk
 
+## Moves a [Chunk] from its `previousPosition` to its `currentPosition`. 
 func updateChunkPosition(chunk: Chunk, currentPosition: Vector2i, previousPosition: Vector2i):
 	if positionToChunk.has(previousPosition):
 		if positionToChunk[previousPosition] == chunk:
@@ -21,6 +25,7 @@ func updateChunkPosition(chunk: Chunk, currentPosition: Vector2i, previousPositi
 	chunkToPosition[chunk] = currentPosition
 	positionToChunk[currentPosition] = chunk
 
+## Finds what [Chunk] the `block` belongs to and updates the [Chunk] to add that block.
 func setBlock(globalPosition: Vector3i, block: Block):
 	var chunkTilePosition = Vector2i(floori(globalPosition.x / float(chunkInstance.dimensions.x)), floori(globalPosition.z / float(chunkInstance.dimensions.z)))
 	if (positionToChunk.has(chunkTilePosition)):
